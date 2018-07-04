6e013bfffb5d90ca1890f5b34f9e83ab02e4411f
Jordan Andrew Thomas <thomas.jordan.andrew@gmail.com>
2018-07-04 21:30:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,7 @@
 var playlist = {'bon jovi': 'living on a prayer'};
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({ artistName: songTitle })
+  playlist =  Object.assign({ artistName: songTitle })
+  console.log(playlist);
 }
 function removeFromPlaylist(playlist, name){
   return delete playlist.name;
